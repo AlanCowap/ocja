35906368ce6cac03a852a375e42472fbab56fac0
Alan Cowap <AlanCowap@users.noreply.github.com>
2014-09-02 12:13:28 +0100

    Update Stacker.java
    Add try-catch to handle errors

--- Stacker.java
+++ Stacker.java
@@ -3,7 +3,7 @@
  *	
  * @author Alan Cowap
  * @version 1.0 Basic class crashing with StackOverFlowError
- * 
+ * @version 1.1 Add Error handling code with try-catch
  * 
  * 
  * 
@@ -14,7 +14,17 @@
 public class Stacker {
 	private static long count = 0L; //count the number of stack frames
 	public static void main(String[] args){
-		new Stacker().doThis();	
+//		new Stacker().doThis();	// this will throw a StackOverFlowError
+
+		try{
+			new Stacker().doThis();
+		}catch(StackOverflowError soe){
+			System.out.println("Oops I'm stacked :) " + count);
+		}
+		catch(Error er){	
+			System.out.println("An Unknown Error Occurred " + count);
+		}		
+		System.out.println("Stack 'em and Rack 'em");
 	}
 
 	private void doThis(){
